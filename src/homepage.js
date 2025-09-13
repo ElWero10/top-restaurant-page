fe07d64b944f9549cf9428c492c3c853ae3b794f
Wero <wero@bluecollarlabs.io>
2025-09-12 22:07:34 -0700

    Add Bg image style header/nav

--- src/homepage.js
+++ src/homepage.js
@@ -2,13 +2,21 @@ import heroImage from  "./assets/images/hero-image.jpg";
 
 export default function createHomepage() {
     const content = document.querySelector("#content");
+    
 
     //Background Image
 
     const background = document.createElement("img");
     background.src = heroImage;
+    background.style.position = "absolute";
+    background.style.zIndex = -1;
     content.appendChild(background);
 
+    //Hero Text
+
+    const heroText = document.createElement("h1");
+    heroText.textContent = "WABURGER";
+    content.appendChild(heroText);
 }
 
 createHomepage()
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
+import "./styles.css";
 import createHomepage from "./homepage.js";
